e4bb57e86b541aa8bf56bf74f7c0be42598e9443
Kyle Enriquez <kyleenriquez1204@gmail.com>
2023-12-26 19:04:08 -0500

    Completed

--- decoder.py
+++ decoder.py
@@ -2,9 +2,12 @@ def main():
     decode("coding_qual_input.txt")
 
 def decode(message_file):
-    with open(message_file) as file:
-        for line in file:
-            print(line)
+    with open(message_file, 'r') as file:
+        lines = file.readlines()
+    numbers = [int(line.split()[0]) for line in lines]
+    words = [line.split()[1] for i, line in enumerate(lines) if i + 1 in numbers]
+    decoded_message = ' '.join(words)
+    return decoded_message
 
 if __name__ == "__main__":
     main()
