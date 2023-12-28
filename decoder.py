0b81d30cab2c02f358573c3412710bfe69f85f62
Kyle Enriquez <kyleenriquez1204@gmail.com>
2023-12-28 17:39:58 -0500

    Final Commit

--- decoder.py
+++ decoder.py
@@ -2,10 +2,19 @@ def main():
     decode("coding_qual_input.txt")
 
 def decode(message_file):
-    with open(message_file, 'r') as file:
-        lines = file.readlines()
-    numbers = [int(line.split()[0]) for line in lines]
-    words = [line.split()[1] for i, line in enumerate(lines) if i + 1 in numbers]
+    with open(message_file) as file:
+        data = []
+        for line in file:
+            line = line.split()
+            data.append(line)
+    sorted_list = sorted(data, key=lambda entry: int(entry[0]))
+    count = 0
+    inc = 1
+    words = []
+    while (count < len(sorted_list)):
+        words.append(sorted_list[count][1])
+        inc = inc + 1
+        count = count + inc
     decoded_message = ' '.join(words)
     return decoded_message
 
